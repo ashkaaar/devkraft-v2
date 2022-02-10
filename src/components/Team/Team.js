b57021a401ffae7ec1481db9aca0dd718ef0b9f4
av1shkar <96016851+av1shkar@users.noreply.github.com>
2022-02-10 12:59:30 +0530

    Update Team.js

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -175,7 +175,7 @@ function Team() {
               isBlog={false}
               title="Parth Tank"
               description="Discord Admin"
-              link="https://www.linkedin.com/in/parth-tank-121934223x"
+              link="www.linkedin.com/in/parth-tank"
             />
           </Col>
           <Col md={4} className="project-card">
